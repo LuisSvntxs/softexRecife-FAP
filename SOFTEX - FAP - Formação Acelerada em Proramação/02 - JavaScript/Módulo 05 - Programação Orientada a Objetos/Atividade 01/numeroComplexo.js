6a2f9a59b3f3671cc881d6774bf575ba9de7f307
LuisSvntxs <luisscopo@gmail.com>
2023-09-16 15:28:03 -0300

    módulo 5 de js

---
+++ SOFTEX - FAP - Formação Acelerada em Proramação/02 - JavaScript/Módulo 05 - Programação Orientada a Objetos/Atividade 01/numeroComplexo.js
@@ -0,0 +1,60 @@
+// Definição do TAD para números complexos
+class NumeroComplexo {
+    constructor(real, imaginario) {
+      this.real = real;
+      this.imaginario = imaginario;
+    }
+  
+    // Método para somar números complexos
+    soma(outroComplexo) {
+      const parteReal = this.real + outroComplexo.real;
+      const parteImaginaria = this.imaginario + outroComplexo.imaginario;
+      return new NumeroComplexo(parteReal, parteImaginaria);
+    }
+  
+    // Método para subtrair números complexos
+    subtracao(outroComplexo) {
+      const parteReal = this.real - outroComplexo.real;
+      const parteImaginaria = this.imaginario - outroComplexo.imaginario;
+      return new NumeroComplexo(parteReal, parteImaginaria);
+    }
+  
+    // Método para multiplicar números complexos
+    multiplicacao(outroComplexo) {
+      const parteReal = this.real * outroComplexo.real - this.imaginario * outroComplexo.imaginario;
+      const parteImaginaria = this.real * outroComplexo.imaginario + this.imaginario * outroComplexo.real;
+      return new NumeroComplexo(parteReal, parteImaginaria);
+    }
+  
+    // Método para dividir números complexos
+    divisao(outroComplexo) {
+      const denominador = outroComplexo.real ** 2 + outroComplexo.imaginario ** 2;
+      const parteReal = (this.real * outroComplexo.real + this.imaginario * outroComplexo.imaginario) / denominador;
+      const parteImaginaria = (this.imaginario * outroComplexo.real - this.real * outroComplexo.imaginario) / denominador;
+      return new NumeroComplexo(parteReal, parteImaginaria);
+    }
+  
+    // Método para imprimir as partes real e imaginária
+    print() {
+      console.log(`Número Complexo: ${this.real} + ${this.imaginario}i`);
+    }
+  }
+  
+  // Exemplo de uso
+  const num1 = new NumeroComplexo(2, 3);
+  const num2 = new NumeroComplexo(1, -1);
+  
+  num1.print(); // Saída: Número Complexo: 2 + 3i
+  num2.print(); // Saída: Número Complexo: 1 - 1i
+  
+  const somaResult = num1.soma(num2);
+  sumResult.print(); // Saída: Número Complexo: 3 + 2i
+  
+  const subtracaoResult = num1.subtracao(num2);
+  subtractResult.print(); // Saída: Número Complexo: 1 + 4i
+  
+  const multiplicacaoResult = num1.multiplicacao(num2);
+  multiplyResult.print(); // Saída: Número Complexo: 5 + 1i
+  
+  const divisaoResult = num1.divisao(num2);
+  divideResult.print(); // Saída: Número Complexo: 2.5 + 2.5i
