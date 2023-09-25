7bf767f5f1215fc3218253e14058849a1db01806
LuisSvntxs <luisscopo@gmail.com>
2023-09-25 08:21:19 -0300

    adicionando o script de sql

---
+++ SOFTEX - FAP - Formação Acelerada em Proramação/outrosCodigos/cadastro-de-carros-rest-api/script.sql
@@ -0,0 +1,14 @@
+create database db_cadastroCarros;
+
+use db_cadastroCarros;
+
+create table carros (
+codigo int primary key auto_increment,
+modelo varchar(30),
+placa varchar(7)
+);
+
+insert into carros (modelo, placa) value ('Toyota Corolla', 'GGG3535');
+insert into carros (modelo, placa) value ('Honda Civic', 'ELV1590');
+
+select * from carros
