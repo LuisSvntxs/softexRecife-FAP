849db79af189d3b8f6e5ea08d08d235ecb364caa
LuisSvntxs <luisscopo@gmail.com>
2023-09-25 11:37:42 -0300

    ajuste no script.sql

--- SOFTEX - FAP - Formação Acelerada em Proramação/outrosCodigos/cadastro-de-carros-rest-api/script.sql
+++ SOFTEX - FAP - Formação Acelerada em Proramação/outrosCodigos/cadastro-de-carros-rest-api/script.sql
@@ -12,3 +12,7 @@ insert into carros (modelo, placa) value ('Toyota Corolla', 'GGG3535');
 insert into carros (modelo, placa) value ('Honda Civic', 'ELV1590');
 
 select * from carros
+
+-- Em caso de erro de conecção 1251, executar a linha de comando abaixo
+
+alter user 'root'@'localhost' identified with mysql_native_password by 'root'; 
